277927ebad15a240d6a244063e90443be3e2cd6d
Sergey Shcherbina <trytoheal@gmail.com>
2018-04-05 15:01:31 +0500

    added DefaultMasterReplicationMiddleware

--- django_replicated/middleware.py
+++ django_replicated/middleware.py
@@ -145,6 +145,26 @@ class ReplicationMiddleware(MiddlewareMixin):
                             max_age=settings.REPLICATED_FORCE_MASTER_COOKIE_MAX_AGE)
 
 
+
+class DefaultMasterReplicationMiddleware(ReplicationMiddleware):
+    def process_request(self, request):
+        if self.forced_state is not None:
+            state = self.forced_state
+            log.debug('state by .forced_state attr: %s', state)
+        elif request.META.get(settings.REPLICATED_FORCE_STATE_HEADER) in ('master', 'slave'):
+            state = request.META[settings.REPLICATED_FORCE_STATE_HEADER]
+            log.debug('state by header: %s', state)
+        else:
+            state = 'master'
+            log.debug('state by request method: %s', state)
+            state = self.check_state_override(request, state)
+            log.debug('state after override: %s', state)
+
+            log.debug('init state: %s', state)
+        routers.init(state)
+
+
+
 class ReadOnlyMiddleware(MiddlewareMixin):
     def process_request(self, request):
         request.service_is_readonly = functional.SimpleLazyObject(self.is_service_read_only)
